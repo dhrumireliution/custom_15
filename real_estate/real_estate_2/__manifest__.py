d740b55562bb61116d7d52b8953d28b6aa7e06aa
dhrumireliution <dhrumip.reliution@gmail.com>
2023-03-15 18:30:39 +0530

    add changes

---
+++ real_estate/real_estate_2/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': '',
+    'depends': ['sale'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
