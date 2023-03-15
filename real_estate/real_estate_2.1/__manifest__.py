c3011c0dbb7e34378c41beff8e078cd391397cac
dhrumireliution <dhrumip.reliution@gmail.com>
2023-03-15 15:05:07 +0530

    add module

---
+++ real_estate/real_estate_2.1/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Real Estate',
+    'version': '1.0.0',
+    'category': '',
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
